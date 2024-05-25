aa50dc9696540deba827b888518111efec981aef
geo037 <giorgos.markoulidakis@gmail.com>
2024-05-25 21:24:55 +0300

    δτην

--- Querry_Test.sql
+++
@@ -1,9 +0,0 @@
-SELECT recipe.rec_name AS name, uses_equipment.equipment_name AS equipment, food_group.recipe_description AS malakia, ingredients.calories_per_100gr AS calories_of_main_ingredient_per_100gr
-FROM recipe
-JOIN uses_equipment ON uses_equipment.rec_name = recipe.rec_name
-JOIN ingredients ON ingredients.name_of_ingredient = recipe.name_of_main_ingredient
-JOIN food_group ON ingredients.name_of_food_group = food_group.name_of_food_group;
-
-
-
--- Recipe Name, equipment, malakia, ?calories to main ingredient
