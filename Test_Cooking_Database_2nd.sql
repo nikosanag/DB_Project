4f4bad69dfcaa1e11c1790f0ebc0966793e0807b
geo037 <giorgos.markoulidakis@gmail.com>
2024-05-26 15:58:05 +0300

    hrj

--- Test_Cooking_Database_2nd.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO equipment
-VALUES ('Mixer', 'No idea how to use this');
-INSERT INTO step
-VALUES ('blah blah blah', 3, 'Chocolate Pie');
-INSERT INTO step
-VALUES ('blah blah blah', 4, 'Chocolate Pie');
-
-SELECT * FROM step;
